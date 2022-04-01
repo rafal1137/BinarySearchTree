d19bc079e77706ac1abad5fdf11a23a153601a2f
rafal1137 <rafal1137@yahoo.pl>
2022-04-01 19:28:31 +0200

    Update header file with functions

--- bst.h
+++ bst.h
@@ -5,3 +5,12 @@ struct node {
   int key;
   struct node *left, *right;
 };
+
+
+// Functions
+
+struct node *newNode(int item);
+void inorder(struct node *root);
+struct node *insert(struct node *node, int key);
+struct node *minValueNode(struct node *node);
+struct node *deleteNode(struct node *root, int key);
