ad8d0116c8dfc563b670d4cdcd779e4df27a23a4
rafal1137 <rafal1137@yahoo.pl>
2022-04-01 19:26:15 +0200

    added main file

---
+++ main.c
@@ -0,0 +1,20 @@
+// Driver code
+int main() {
+  struct node *root = NULL;
+  root = insert(root, 8);
+  root = insert(root, 3);
+  root = insert(root, 1);
+  root = insert(root, 6);
+  root = insert(root, 7);
+  root = insert(root, 10);
+  root = insert(root, 14);
+  root = insert(root, 4);
+
+  printf("Inorder traversal: ");
+  inorder(root);
+
+  printf("\nAfter deleting 10\n");
+  root = deleteNode(root, 10);
+  printf("Inorder traversal: ");
+  inorder(root);
+}
